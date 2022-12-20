e5f2eb9583d90958bb6872ede756e25c532f7bda
Marcelo Schambeck <marcelo.moraes@navita.com.br>
2022-12-20 04:58:04 -0300

    Running tests with H2 database

--- app/src/main/java/com/schambeck/erp/sales/app/dataprovider/entity/OrderEntity.java
+++ app/src/main/java/com/schambeck/erp/sales/app/dataprovider/entity/OrderEntity.java
@@ -3,10 +3,7 @@ package com.schambeck.erp.sales.app.dataprovider.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.*;
@@ -16,7 +13,7 @@ import static javax.persistence.CascadeType.ALL;
 @Getter
 @Setter
 @Entity
-@Table(name = "order")
+@Table(name = "ORDER")
 public class OrderEntity {
     public OrderEntity() {
     }
@@ -26,12 +23,16 @@ public class OrderEntity {
     }
 
     @Id
+    @Column(name = "ID")
     @NotNull(message = "Id is required")
     private UUID id;
+    @Column(name = "CLIENT_ID")
     @NotNull(message = "Client is required")
     private UUID clientId;
+    @Column(name = "STATUS")
     @NotNull(message = "Status is required")
     private StatusOrder status;
+    @Column(name = "TOTAL_COST")
     @NotNull(message = "Total cost is required")
     private BigDecimal totalCost;
     @OneToMany(cascade = ALL, orphanRemoval = true, mappedBy = "order")
--- app/src/main/java/com/schambeck/erp/sales/app/dataprovider/entity/OrderLineEntity.java
+++ app/src/main/java/com/schambeck/erp/sales/app/dataprovider/entity/OrderLineEntity.java
@@ -13,20 +13,25 @@ import java.util.UUID;
 @Setter
 @ToString
 @Entity
-@Table(name = "order_line")
+@Table(name = "ORDER_LINE")
 public class OrderLineEntity {
     @Id
+    @Column(name = "ID")
     @GeneratedValue
     private UUID id;
     @ManyToOne
-    @JoinColumn(name = "order_id")
+    @JoinColumn(name = "ORDER_ID")
     @NotNull(message = "Order is required")
     private OrderEntity order;
+    @Column(name = "PRODUCT_ID")
     @NotNull(message = "Product is required")
     private UUID productId;
+    @Column(name = "QUANTITY")
     @NotNull(message = "Quantity is required")
     private BigDecimal quantity;
+    @Column(name = "PRICE")
     @NotNull(message = "Price is required")
     private BigDecimal price;
+    @Column(name = "COST")
     private BigDecimal cost;
 }
