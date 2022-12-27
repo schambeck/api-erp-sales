2bda11925425e5ab339ed8bf65c18987874d7591
Marcelo Schambeck <marcelo.moraes@navita.com.br>
2022-12-27 02:25:08 -0300

    IT suffix

--- app/src/test/java/com/schambeck/erp/sales/app/SalesApplicationIT.java
+++ app/src/test/java/com/schambeck/erp/sales/app/SalesApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class SalesApplicationTests {
+class SalesApplicationIT {
 	@Autowired
 	private SalesApplication context;
 
