76f47dc2432d2204eea4f36ec5aead22d608ae91
Marcelo Schambeck <marcelo.moraes@navita.com.br>
2023-01-09 01:15:43 -0300

    EqualsVerifier OrderMsgTest

---
+++ app/src/test/java/com/schambeck/erp/sales/app/dataprovider/model/OrderMsgTest.java
@@ -0,0 +1,16 @@
+package com.schambeck.erp.sales.app.dataprovider.model;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+
+import static nl.jqno.equalsverifier.Warning.ALL_FIELDS_SHOULD_BE_USED;
+
+class OrderMsgTest {
+    @Test
+    void equalsContract() {
+        EqualsVerifier.simple()
+                .suppress(ALL_FIELDS_SHOULD_BE_USED)
+                .forClass(OrderMsg.class)
+                .verify();
+    }
+}
