d97decaf3218b5308d953cd52d769d8741dbd378
Marcelo Schambeck <marcelo.moraes@navita.com.br>
2023-01-09 01:04:20 -0300

    ObjectMapper jsr310 JavaTimeModule

--- app/src/main/java/com/schambeck/erp/sales/app/entrypoint/messaging/RabbitConfig.java
+++ app/src/main/java/com/schambeck/erp/sales/app/entrypoint/messaging/RabbitConfig.java
@@ -1,5 +1,7 @@
 package com.schambeck.erp.sales.app.entrypoint.messaging;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -7,6 +9,7 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 class RabbitConfig {
@@ -30,16 +33,17 @@ class RabbitConfig {
     String xDeadLetterRoutingKey;
 
     @Bean
-    RabbitTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+    RabbitTemplate amqpTemplate(ConnectionFactory connectionFactory, Jackson2ObjectMapperBuilder builder) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setChannelTransacted(true);
-        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter(builder));
         return rabbitTemplate;
     }
 
     @Bean
-    Jackson2JsonMessageConverter producerJackson2MessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    Jackson2JsonMessageConverter producerJackson2MessageConverter(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.build().registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 
     @Bean
