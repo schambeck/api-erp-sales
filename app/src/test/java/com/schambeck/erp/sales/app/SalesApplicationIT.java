3c134338044256c49a145452a99c322f6b43ffa4
Marcelo Schambeck <marcelo.moraes@navita.com.br>
2023-01-09 01:01:24 -0300

    Rabbit test mock

--- app/src/test/java/com/schambeck/erp/sales/app/SalesApplicationIT.java
+++ app/src/test/java/com/schambeck/erp/sales/app/SalesApplicationIT.java
@@ -1,12 +1,15 @@
 package com.schambeck.erp.sales.app;
 
+import com.schambeck.erp.sales.app.config.RabbitTestConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@Import(RabbitTestConfiguration.class)
 class SalesApplicationIT {
 	@Autowired
 	private SalesApplication context;
